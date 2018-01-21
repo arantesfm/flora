6bdb76be614aeecf475be4a466144a2d6e12828d
samuel.sampaio <samuel.sampaio@tivit.com>
2018-01-21 18:53:12 -0200

    Adicionado paginas: campus, projeto, herbario, categoria de especies e listagem de especies

--- flora/urls.py
+++ flora/urls.py
@@ -2,12 +2,22 @@ from django.conf.urls import include, url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    url(r'^$', views.post_list),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
     url(r'^post/new/$', views.post_new, name='post_new'),
-    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'), 
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 
+    url(r'^projeto/', views.project),
+    url(r'^campus/', views.campus),
+    url(r'^hrcb/', views.hrcb),
+
+    url(r'^categorias/', views.species_categories),
+    url(r'^especies/categoria/nome/', views.list_species_by_name),
+    url(r'^especies/categoria/familia/', views.list_species_by_family),
+    url(r'^especies/categoria/genero/', views.list_species_by_genre),
+    url(r'^especies/categoria/especie/', views.list_species_by_specie),
+
+    url(r'^$', RedirectView.as_view(url='/especies/categoria/nome/')),
 ]
-
--- flora/views.py
+++ flora/views.py
@@ -5,13 +5,47 @@ from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'flora/post_list.html', {'posts': posts})
+
+def project(request):
+    return render(request, 'flora/project.html')
+
+
+def campus(request):
+    return render(request, 'flora/campus.html')
+
+
+def hrcb(request):
+    return render(request, 'flora/hrcb.html')
+
+
+def species_categories(request):
+    return render(request, 'flora/species_categories.html')
+
+
+def list_species_by_name(request):
+    species = Post.objects.all().order_by('title')
+    return render(request, 'flora/list_species.html', {'species': species, 'view': 'Nome'})
+
+
+def list_species_by_family(request):
+    species = Post.objects.all().order_by('familia')
+    return render(request, 'flora/list_species.html', {'species': species, 'view': 'Família'})
+
+
+def list_species_by_genre(request):
+    species = Post.objects.all().order_by('genero')
+    return render(request, 'flora/list_species.html', {'species': species, 'view': 'Gênero'})
+
+
+def list_species_by_specie(request):
+    species = Post.objects.all().order_by('especie')
+    return render(request, 'flora/list_species.html', {'species': species, 'view': 'Espécie'})
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'flora/post_detail.html', {'post': post})
+
 
 def post_new(request):
     if request.method == "POST":
@@ -26,6 +60,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'flora/post_edit.html', {'form': form})
 
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -38,4 +73,4 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'flora/post_edit.html', {'form': form}) 
+    return render(request, 'flora/post_edit.html', {'form': form})
